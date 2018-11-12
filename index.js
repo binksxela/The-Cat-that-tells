bcbf325498b7f62097290ba4c486b38279ecddf8
Acaretia <merle.adel@hotmail.com>
2018-11-12 19:10:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ Cat
 })
 .on('message', message => {
 
+    // If the message was sent by a bot, it's ignored
     if(message.author.bot) return
 
+    // Ignore all messages was sent in a specific channel
     if(message.channel === channel) return
     for(let i = 0; i < ignoredChannels.length; i++)
         if(message.channel === Cat.channels.get(ignoredChannels[i])) return
 
+    // Check if the message contain a role mention
     if(message.mentions.roles.first()) {
         const embed = new RichEmbed()
         .setColor([122, 226, 65])
@@ -24,11 +27,14 @@ Cat
         .addField('Content', message.content ? message.content.substr(0, 500) : '❌ _Image(s)_')
         .setFooter(`Date: ${message.createdAt}`)
 
+        // Log the message + the author in the log's channel
         return channel.send({ embed })
     }
 
 })
 .on('messageDelete', message => {
+    
+    // Same thing for the event 'message'
 
     if(message.author.bot) return
 
