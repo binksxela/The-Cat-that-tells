f94d2558d855d2ba3614fe4978040da38da5a4ce
Adel <mrl.adel.sanorel@gmail.com>
2018-11-12 21:29:14 +0100

    Update

--- index.js
+++ index.js
@@ -6,59 +6,43 @@ const { Client, RichEmbed } = require('discord.js')
 const Cat = new Client()
 let channel
 
-Cat
-.on('ready', () => {
-    channel = Cat.channels.get(channelId)   
-    console.log("The cat want to tell a story")
-})
-.on('message', message => {
-
+const toLog = (message) => {
+    // If the message was sent by a bot, it's ignored
     if(message.author.bot) return
 
+    // Ignore all messages was sent in a specific channel
     if(message.channel === channel) return
     for(let i = 0; i < ignoredChannels.length; i++)
         if(message.channel === Cat.channels.get(ignoredChannels[i])) return
 
+    // Check if the message contain a role mention
     if(message.mentions.roles.first()) {
         const embed = new RichEmbed()
         .setColor([122, 226, 65])
         .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
         .addField('Channel', `<#${message.channel.id}> (${message.channel.name ? message.channel.name : 'ERROR'})`, true)
-        .addField('Content', message.content ? message.content.substr(0, 500) : "❌ _Image(s)_")
+        .addField('Content', message.content ? message.content.substr(0, 500) : '❌ _Image(s)_')
         .setFooter(`Date: ${message.createdAt}`)
 
+        // Log the message + the author in the log's channel
         return channel.send({ embed })
     }
+}
 
+Cat
+.on('ready', () => {
+    channel = Cat.channels.get(channelId)   
+
+    app.set('port', (process.env.PORT || 5000))
+    app.get('/', (request, response) => {
+        response.sendFile('index.html')
+    }).listen(app.get('port'), () => {
+        console.log("App is running, server is listening on port ", app.get('port'))
+    })
+
+    console.log('The cat want to tell a story')
 })
-.on('messageDelete', message => {
-
-    if(message.author.bot) return
-
-    if(message.channel === channel) return
-    for(let i = 0; i < ignoredChannels.length; i++)
-        if(message.channel === Cat.channels.get(ignoredChannels[i])) return
-
-    if(message.mentions.roles.first()) {
-        const embed = new RichEmbed()
-        .setColor([244, 95, 66])
-        .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
-        .addField('Channel', `<#${message.channel.id}> (${message.channel.name ? message.channel.name : 'ERROR'})`, true)
-        .addField('Content', message.content ? message.content.substr(0, 500) : "❌ _Image(s)_")
-        .setFooter(`Date: ${message.createdAt}`)
-
-        return channel.send({ embed })
-    }
-
-})
+.on('message', message => toLog(message))
+.on('messageDelete', message => toLog(message))
 
 Cat.login(token)
-
-app.set('port', (process.env.PORT || 5000))
-
-app.get('/', (request, response) => {
-    let result = "App is running | le Chat qui raconte"
-    response.send(result)
-}).listen(app.get('port'), () => {
-    console.log("App is running, server is listening on port ", app.get('port'))
-});
