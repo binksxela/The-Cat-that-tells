c3103beb3a0261e00cb1cce312080ea72f826ebd
Acaretia <merle.adel@hotmail.com>
2018-11-17 18:58:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const toLog = (channel) => (message) => {
     // Check if the message contain a role mention
     const embed = new RichEmbed()
         .setColor([122, 226, 65])
-        .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+        .setAuthor(`${message.author.tag}`, message.author.avatarURL)
         .addField('Channel', `${message.channel} (${message.channel.name || 'ERROR'})`, true)
         .addField(`Go to the message`, `[Link](${message.url})`, true)
         .addField('Content', message.content ? message.content.substr(0, 500) : '❌ _Image(s)_')
