dc3f4d43ced98649abf982ea80407d2bda05a358
Acaretia <merle.adel@hotmail.com>
2018-11-17 18:55:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const toLog = (channel) => (message) => {
     const embed = new RichEmbed()
         .setColor([122, 226, 65])
         .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
-        .addField('Channel', `${message.channel.toString()} (${message.channel.name ? message.channel.name : 'ERROR'})`, true)
+        .addField('Channel', `${message.channel} (${message.channel.name || 'ERROR'})`, true)
         .addField(`Go to the message`, `[Link](${message.url})`, true)
         .addField('Content', message.content ? message.content.substr(0, 500) : '❌ _Image(s)_')
         .setTimestamp(message.createdAt)
